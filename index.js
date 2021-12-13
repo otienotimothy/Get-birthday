477d51e798443b422570b21104f3dadd79c96311
Timothy Otieno <timothyking094@gmail.com>
2021-12-13 15:45:17 +0300

    add functionality to collect date of birth from user

---
+++ index.js
@@ -0,0 +1,16 @@
+/** Get Akan Name */
+
+function getAkanName(e) {
+    e.preventDefault();
+
+    //Get Date of Birth Value from Form
+    let dateOfBirth = document.querySelector('.date').value;
+    //Create a date object from the value
+    let dateObj = new Date(dateOfBirth);
+    //Return Day of the Week from the object - Returns integers 0-6 with 0 being Sunday
+    let dayOfWeek = dateObj.getDay();
+
+    
+
+}
+
