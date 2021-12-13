bd5cfe656c935d0e30a1011454b27b474121a0e3
Timothy Otieno <timothyking094@gmail.com>
2021-12-13 15:55:15 +0300

    add a list of male and female Akan names

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ function getAkanName(e) {
     let dateOfBirth = document.querySelector('.date').value;
     //Create a date object from the value
     let dateObj = new Date(dateOfBirth);
-    //Return Day of the Week from the object - Returns integers 0-6 with 0 being Sunday
+    //Return Day of the Week from the date object - Returns integers 0-6 with 0 being Sunday
     let dayOfWeek = dateObj.getDay();
 
     //Get User Gender
     let gender = document.querySelector('.gender').value;
+
+    let maleNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+    let femaleName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+
+    
 
 }
 
